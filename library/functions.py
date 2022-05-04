fecd3a1ccb445f61876c1f7ef847e96d3a3bb126
Jonathan Leeming <jpleeming51@gmail.com>
2022-05-04 20:09:33 +0100

    Added utilities for functions

---
+++ library/functions.py
@@ -0,0 +1,11 @@
+"""The controller for the interactive prompt window"""
+
+__author__ = 'Jonathan Leeming'
+__version__ = '0.1'
+__all__ = ['unused']
+
+from typing import Any
+
+
+def unused(*_: Any, **__: Any) -> None:
+    """Mark variables as unused"""
