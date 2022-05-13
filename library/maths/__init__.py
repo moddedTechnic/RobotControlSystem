57866db8d7ba2980a0d470be23b1081538b78957
Jonathan Leeming <jpleeming51@gmail.com>
2022-05-13 13:37:28 +0100

    Added the functionality to calculate the GCD of two numbers

--- library/maths/__init__.py
+++ library/maths/__init__.py
@@ -2,4 +2,16 @@
 
 __author__ = 'Jonathan Leeming'
 __version__ = '0.1'
-__all__ = []
+__all__ = ['gcd']
+
+
+def gcd(a: int, b: int) -> int:
+    """Find the greatest common divisor of a and b."""
+    x, y = abs(a), abs(b)
+    if x == 0:
+        return y
+    if y == 0:
+        return x
+    if a < 0 and b < 0:
+        return gcd(y, x % y) * -1
+    return gcd(y, x % y)
